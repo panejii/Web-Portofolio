4e4f11773700c42e8cabc14162b7945e38a8d209
panejii <faneziaja@gmail.com>
2026-07-06 12:21:43 +0700

    feat: integrating login page with backend so user can login as admin

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,76 +1,90 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { login } from "../services/authService";
 
 const Login = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const navigate = useNavigate();
 
-  const handleLogin = (e) => {
-    e.preventDefault();
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState("");
 
-    console.log({
-      email,
-      password,
-    });
-  };
+    const handleLogin = async (e) => {
+        e.preventDefault();
 
-  return (
-    <div className="min-h-screen bg-zinc-900 flex items-center justify-center px-4">
-      <div className="w-full max-w-md bg-zinc-700 rounded-xl shadow-lg p-8">
+        try {
+            const data = await login(email, password);
 
-        <div className="text-center mb-8">
-          <h1 className="text-3xl font-bold text-white">
-            Admin Login
-          </h1>
+            console.log(data);
 
-          <p className="text-white/50 mt-2">
-            Silakan masuk untuk mengelola portofolio
-          </p>
+            navigate("/admin");
+
+        } catch (error) {
+            console.error(error);
+
+            alert("Email atau Password salah");
+        }
+    };
+
+    return (
+        <div className="min-h-screen bg-zinc-900 flex items-center justify-center px-4">
+        <div className="w-full max-w-md bg-zinc-700 rounded-xl shadow-lg p-8">
+
+            <div className="text-center mb-8">
+            <h1 className="text-3xl font-bold text-white">
+                Admin Login
+            </h1>
+
+            <p className="text-white/50 mt-2">
+                Silakan masuk untuk mengelola portofolio
+            </p>
+            </div>
+
+            <form
+            onSubmit={handleLogin}
+            className="space-y-5"
+            >
+
+                <div>
+                    <label className="block mb-2 font-medium text-white">
+                    Email
+                    </label>
+
+                    <input
+                    type="email"
+                    placeholder="example@gmail.com"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    className="w-full border rounded-lg px-4 py-3 outline-none focus:ring-2 focus:ring-blue-500"
+                    />
+                </div>
+
+                <div>
+                    <label className="block mb-2 font-medium text-white">
+                    Password
+                    </label>
+
+                    <input
+                    type="password"
+                    placeholder="••••••••"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    className="w-full border rounded-lg px-4 py-3 outline-none focus:ring-2 focus:ring-blue-500"
+                    />
+                </div>
+
+                <button
+                    type="submit"
+                    className="w-full bg-violet-700 text-white py-3 rounded-lg hover:bg-violet-600 transition"
+                >
+                    Login
+                </button>
+
+            </form>
         </div>
-
-        <form
-          onSubmit={handleLogin}
-          className="space-y-5"
-        >
-
-          <div>
-            <label className="block mb-2 font-medium text-white">
-              Email
-            </label>
-
-            <input
-              type="email"
-              placeholder="example@gmail.com"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              className="w-full border rounded-lg px-4 py-3 outline-none focus:ring-2 focus:ring-blue-500"
-            />
-          </div>
-
-          <div>
-            <label className="block mb-2 font-medium text-white">
-              Password
-            </label>
-
-            <input
-              type="password"
-              placeholder="••••••••"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              className="w-full border rounded-lg px-4 py-3 outline-none focus:ring-2 focus:ring-blue-500"
-            />
-          </div>
-
-          <button
-            type="submit"
-            className="w-full bg-violet-700 text-white py-3 rounded-lg hover:bg-violet-600 transition"
-          >
-            Login
-          </button>
-
-        </form>
-      </div>
-    </div>
-  );
+        </div>
+    );
 };
 
 export default Login;
